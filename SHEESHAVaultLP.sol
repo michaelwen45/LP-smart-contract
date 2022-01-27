49ec81c4a9a4bb55858e5de376fe4c2cf5efe495
“MichaelWen <michaelwen.dev@gmail.com>
2022-01-27 23:20:43 +0300

    update on vaultLP smart contract

--- SHEESHAVaultLP.sol
+++ SHEESHAVaultLP.sol
@@ -16,4 +16,18 @@ contract SHEESHAVaultLP is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
     using SafeERC20 for SHEESHA;
+
+    struct UserInfo {
+        uint256 amount;
+        uint256 rewardDebt;
+        uint256 checkpoint;
+        bool status;
+    }
+
+    struct PoolInfo {
+        IERC20 lpToken;
+        uint256 allocPoint;
+        uint256 lastRewardBlock;
+        uint256 accSheeshaPerShare;
+    }
 }
